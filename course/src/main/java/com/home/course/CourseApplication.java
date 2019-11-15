3b37cafd0aa6174d48253e1a9eaf297aaf1b4eec
Aman7490 <amandeep7490@gmail.com>
2019-11-15 15:51:24 +0530

    Swagger Implemented

--- course/src/main/java/com/home/course/CourseApplication.java
+++ course/src/main/java/com/home/course/CourseApplication.java
@@ -3,7 +3,10 @@ package com.home.course;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class CourseApplication {
 
 	public static void main(String[] args) {
